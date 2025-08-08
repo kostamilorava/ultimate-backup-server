0b4bafcaf6c92534e0ebc71330554b7faedcebea
kostamilorava <kosta.milorava@gmail.com>
2025-08-08 23:16:10 +0400

    remove inspire command

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,5 @@
 <?php
-
-
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Spatie\BackupServer\Tasks\Summary\Jobs\SendServerSummaryNotificationJob;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 
 Schedule::command('backup-server:dispatch-backups')->everyMinute();
